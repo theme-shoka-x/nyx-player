33ac20e4feb51dbcdbf92496239bb017f1e8229b
zkz098 <zkz20081204@126.com>
2025-03-21 19:49:27 +0800

    feat: init函数更新

--- src/entries/init.ts
+++ src/entries/init.ts
@@ -1,3 +1,4 @@
+import type { Preset } from '../presets'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp, h } from 'vue'
@@ -9,6 +10,9 @@ export function initPlayer(
   showBtn: string,
   urls: { url: string, name: string }[],
   playBtn?: string,
+  darkModeTarget?: string,
+  preset?: string,
+  styles?: Preset,
 ) {
   if (!el || !showBtn) {
     throw new Error('el and showBtn are required')
@@ -19,7 +23,7 @@ export function initPlayer(
   if (!player) {
     throw new Error('el must be valid selectors')
   }
-  const app = createApp(() => h(App, { urls, showBtn, playBtn }))
+  const app = createApp(() => h(App, { urls, showBtn, playBtn, darkModeTarget, preset, styles }))
   app.use(createPinia().use(piniaPluginPersistedstate))
   app.mount(player)
 }
